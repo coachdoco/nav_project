84c6fdb1efec39fac9e5fcc07b007023199317f8
coachdoco <70187477+coachdoco@users.noreply.github.com>
2020-11-22 16:01:22 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  burger.addEventListener('click',()=> {
+    //toggle nav
+    nav.classList.toggle('nav-active');
+
+    // animate links
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ""
+    } else {
+      link.style.animation = `navLinkFade 2s ease forwards ${index + 0.3}s`;
+    }
+
+    });
+    //burger animation
+    burger.classList.toggle('toggle');
+  });
+  
+}
+const app = () => {
+  navSlide();
+}
+
+app();
